eea855ba3d3e840449f5ee3023f482a00b00b789
0tuedon <tuedonmofe@gmail.com>
2026-03-23 01:12:04 +0100

    chore: 01-05.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
+
+bitcoin-cli -regtest getrawchangeaddress bech32m
--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,6 @@
 # Create a SegWit address.
+NEW_SEGWIT=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 # Add funds to the address.
+supress=$(bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 102 $NEW_SEGWIT)
 # Return only the Address
+bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $NEW_SEGWIT | jq -r '.address'
